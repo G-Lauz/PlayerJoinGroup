9732784221c0931a4853d7e41021c16839472944
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2022-01-03 16:42:35 -0500

    Handle server switch

--- bungee/src/main/java/fr/freebuild/playerjoingroup/bungee/listener/PlayerSwitchListener.java
+++ bungee/src/main/java/fr/freebuild/playerjoingroup/bungee/listener/PlayerSwitchListener.java
@@ -2,14 +2,11 @@ package fr.freebuild.playerjoingroup.bungee.listener;
 
 import fr.freebuild.playerjoingroup.bungee.PlayerJoinGroup;
 import fr.freebuild.playerjoingroup.bungee.ServerGroupNotFoundException;
+import fr.freebuild.playerjoingroup.bungee.Utils;
 import fr.freebuild.playerjoingroup.core.event.EventType;
 
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.connection.Server;
-import net.md_5.bungee.api.event.ServerConnectEvent;
-import net.md_5.bungee.api.event.ServerConnectedEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.event.EventHandler;
 
@@ -29,8 +26,13 @@ public class PlayerSwitchListener extends ConnectionListener {
             // TODO remove logs?
             System.out.print(player.getName() + " switched from " + fromServer.getName() + " to " + player.getServer().getInfo().getName());
 
-            scheduledBroadcastEvent(fromServer, player, EventType.LEAVE_SERVER_GROUP, 1);
-            scheduledBroadcastEvent(player.getServer().getInfo(), player, EventType.JOIN_SERVER_GROUP, 1);
+            String fromGroup = Utils.getServerGroupName(fromServer.getName(), this.plugin.getConfig());
+            String toGroup = Utils.getServerGroupName(player.getServer().getInfo().getName(), this.plugin.getConfig());
+
+            if (!fromGroup.equalsIgnoreCase(toGroup)) {
+                scheduledBroadcastEvent(fromServer, player, EventType.LEAVE_SERVER_GROUP, 1);
+                scheduledBroadcastEvent(player.getServer().getInfo(), player, EventType.JOIN_SERVER_GROUP, 1);
+            }
         }
     }
 }
