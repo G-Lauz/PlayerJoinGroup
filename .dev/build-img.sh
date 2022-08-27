2aee41d592422d36d3e05e15c96fedeec0c36dd8
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2022-08-27 09:14:17 -0400

    Add docker-compose support

--- .dev/build-img.sh
+++ .dev/build-img.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f ./spigot/Dockerfile spigot-server:latest --build-arg MINECRAFT_VERSION=1.19.2 .
+docker build -f ./spigot/Dockerfile spigot-server:1.19.2 --build-arg MINECRAFT_VERSION=1.19.2 .
 docker build -f ./bungee/Dockerfile bungee-server:latest .
---
+++ .dev/compose.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose -f .\.dev\compose.yaml -p playerjoingroup-dev up --build --force-recreate
